20cbc7b93dd842fd2c8ad7e48e2d5705fe853f47
Yevhen Strohanov <yevhen.strohanov@fiserv.com>
2020-01-14 13:38:15 -0600

    Initial Commit

---
+++ FetchRewardsAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FetchRewardsAssignment
+//
+//  Created by Strohanov, Yevhen (Chicago) on 1/14/20.
+//  Copyright © 2020 Organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
